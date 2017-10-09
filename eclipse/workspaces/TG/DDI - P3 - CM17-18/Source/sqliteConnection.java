3a750bf2bd667b45085af10d9d77bf172e5db0a6
alu01-dam2 <alu01-dam2@alu17-dam2>
2017-10-09 12:24:22 +0200

    sql

---
+++ eclipse/workspaces/TG/DDI - P3 - CM17-18/Source/sqliteConnection.java
@@ -0,0 +1,24 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+import javax.swing.JOptionPane;
+
+public class sqliteConnection {
+	Connection conn = null;
+	
+	public static Connection dbConnector() {
+		
+		try {
+			
+			Class.forName("org.sqlite.JDBC");
+			Connection conn = DriverManager.getConnection("jdbc:sqlite:bd\\jugadores.sqlite");
+			JOptionPane.showMessageDialog(null, "Conexión establecida");		
+			return conn;
+			
+		}catch(Exception e) {
+			JOptionPane.showMessageDialog(null, e);
+			return null;
+		}
+		
+	}
+}
