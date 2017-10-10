9ff3f4c6a950dbfdf396414316050101b9a1da7c
Alvaro Valera Lopez <alvarovaleralopez@gmail.com>
2017-10-10 09:24:29 +0200

    ELiminacion de redundancias

--- eclipse/workspaces/TG/DDI - P3 - CM17-18/Source/Modelo/sqliteConnection.java
+++ eclipse/workspaces/TG/DDI - P3 - CM17-18/Source/Modelo/sqliteConnection.java
@@ -6,21 +6,20 @@ import java.sql.DriverManager;
 import javax.swing.JOptionPane;
 
 public class sqliteConnection {
-	Connection conn = null;
-	
+
 	public static Connection dbConnector() {
-		
+
 		try {
-			
+
 			Class.forName("org.sqlite.JDBC");
 			Connection conn = DriverManager.getConnection("jdbc:sqlite:db//jugadores.sqlite");
-			JOptionPane.showMessageDialog(null, "Conexi�n establecida");		
+			JOptionPane.showMessageDialog(null, "Conexión establecida");
 			return conn;
-			
-		}catch(Exception e) {
+
+		} catch (Exception e) {
 			JOptionPane.showMessageDialog(null, e);
 			return null;
 		}
-		
+
 	}
 }
